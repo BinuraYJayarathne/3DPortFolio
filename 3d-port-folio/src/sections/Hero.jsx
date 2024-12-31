ab95dfa6f1c76d3864bf956cc4385e1b5bd3563b
BinuraYJayarathne <binurayj88@gmail.com>
2024-12-31 17:38:59 +0530

    Day 3 progress

--- 3d-port-folio/src/sections/Hero.jsx
+++ 3d-port-folio/src/sections/Hero.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { Suspense } from 'react';
+import HackerRoom from '../Components/HackerRoom';
+import CanvasLoader from '../Components/CanvasLoader';
+import { PerspectiveCamera } from '@react-three/drei';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className='min-h-screen w-full flex flex-col relative'>
+      <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3 justify-center items-center text-center'>
+        <p className='sm:text-3xl text-2xl font-medium text-white font-generalsans'>
+          Hi, I am Binura <span className='waving-hand'>👋</span>
+        </p>
+        <p className='hero_tag text-gray_gradient'>
+          Building products and Brands
+        </p>
+      </div>
 
-export default Hero
+      <div className='w-full h-full absolute inset-0'>
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader/>}>
+            <PerspectiveCamera makeDefault position={[0,0,30]}/>
+            <HackerRoom/>
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
